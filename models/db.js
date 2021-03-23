eacbff36648aaae3321b315f7cf33869b8a3b10c
shahidone <shahidone@hotmail.com>
2021-03-24 00:42:37 +0500

    db live

--- models/db.js
+++ models/db.js
@@ -1,10 +1,10 @@
 const mysql = require("mysql");
 
 const db = mysql.createConnection({
-  host: "127.0.0.1",
-  user: "root",
-  password: "",
-  database: "scrapper_new",
+  host: "localhost",
+  user: "mughalfrazk'@'localhost",
+  password: "Password@1",
+  database: "scrapper_live",
 });
 
 module.exports = db;
